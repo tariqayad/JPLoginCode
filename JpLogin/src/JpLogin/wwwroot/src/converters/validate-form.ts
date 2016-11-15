0d710de28ceb6083bc56fb6854c896bb5a28ec58
Tariq <tariqa@navantis.com>
2016-11-15 14:53:13 +0530

    validationMessage

--- JpLogin/src/JpLogin/wwwroot/src/converters/validate-form.ts
+++ JpLogin/src/JpLogin/wwwroot/src/converters/validate-form.ts
@@ -2,6 +2,9 @@
 
 export class ValidateFormValueConverter {
     public toView(validator: ValidationHelper, username: string, password: string): boolean {
+        if (username == "" && password == "") {
+            return true;
+        }
         if (validator.isPasswordValid(password) && validator.isUserNameValid(username)) {
             return true;
         }        
