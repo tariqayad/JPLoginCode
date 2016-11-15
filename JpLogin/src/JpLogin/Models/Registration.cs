07e7227448c1c7bcdea79e078d45d4404091a162
Tariq <tariqa@navantis.com>
2016-11-15 18:03:57 +0530

    writing webapi methods

---
+++ JpLogin/src/JpLogin/Models/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JpLogin.Models
+{
+    public class Registration
+    {
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
---
+++ JpLogin/src/JpLogin/Services/RegistrationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JpLogin.Models;
+
+namespace JpLogin.Services
+{
+    public class RegistrationService
+    {
+        public void RegisterUser(Registration userRegisteration)
+        {
+
+        }
+
+        public bool DoesUserExist(string userName)
+        {
+            return false;
+        }
+
+        public bool IsRegistrationValid(Registration userRegistration)
+        {
+            return false;
+        }
+
+    }
+}
