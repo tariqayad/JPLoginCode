cc7ee2d249820a50b79e908b05438b721892f568
tariq <tariqayad@gmail.com>
2016-11-16 09:18:09 +0530

    azure storage stub

---
+++ JpLogin/src/JpLogin/DataAccess/AzureTableStorageRepository.cs
@@ -0,0 +1,27 @@
+﻿using JpLogin.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JpLogin.Models;
+
+namespace JpLogin.DataAccess
+{
+    public class AzureTableStorageRepository : IDataRepository
+    {
+        public bool DoesRegistrationExist(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Registration GetRegistration(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Registration registration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
