f8739434e0ca1be6eb43b6bbcc47858bd10410d7
tariq <tariqayad@gmail.com>
2016-11-16 06:31:34 +0530

    created interface for data

---
+++ JpLogin/src/JpLogin/Interfaces/IDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using JpLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JpLogin.Interfaces
+{
+    interface IDataRepository
+    {
+        Registration GetRegistration(string username);
+        void Register(Registration registration);
+        bool DoesRegistrationExist(string username);
+    }
+}
